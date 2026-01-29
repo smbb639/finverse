6c2ad4061a2ea16229d6580dad6eed5b1818bb75
smbb639 <mishrashubham0268@gmail.com>
2026-01-29 17:52:48 +0530

    ..

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -10,7 +10,7 @@ async function connection() {
   await connectDB();
 
   app.listen(PORT, () => {
-    console.log(`Server is running on the port ${PORT}`);
+    console.log(`Server is running on the port ${PORT}`); 
     
   });
 }
